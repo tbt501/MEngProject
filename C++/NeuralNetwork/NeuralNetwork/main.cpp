f295e961f44c6610ccbc306c137d85182e46bde3
tbt501 <tbt501@york.ac.uk>
2018-04-21 19:45:25 +0100

    C++ Neural Network Files
    Added an Xcode project for the C++ Neural Network

---
+++ C++/NeuralNetwork/NeuralNetwork/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  NeuralNetwork
+//
+//  Created by Tom Turner on 21/04/2018.
+//  Copyright © 2018 Tom Turner. All rights reserved.
+//
+
+#include <iostream>
+
+#include "/Users/Angelo555uk/Desktop/University/Year_4/Project/Project/OpenNN/opennn/opennn.h"
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
